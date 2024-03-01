97f419749add1177b7988515fc5ca08d4d36dc4e
Hai Long <longdh2.dev@gmail.com>
2024-03-01 11:57:35 +0700

    imporve router

--- router/index.php
+++ router/index.php
@@ -2,31 +2,50 @@
 use System\Core\Router;
 
 class ConfigRouter extends Router {
-    protected $default = 'web';
+    private $default = 'web';
+    private $pathArray = [];
 
     public function __construct()
     {
-        require_once "$this->default.php";
+        require_once __DIR__ROOT."/router/$this->default.php";
     }
 
     public function loadFile($name) {
-        if(file_exists("router/$name.php")) {
-            require_once "$name.php";
+        if(file_exists(__DIR__ROOT."/router/$name.php")) {
+            require_once __DIR__ROOT."/router/$name.php";
         } else {
             http_response_code(500);
-            die("File $name in router does not exit");
+            throw new \RuntimeException("File $name in router does not exit", 500);
+            return;
         }
         self::$path_load_file = '';
         return $this;
     }
 
     public function add($name) {
+        if(is_array($name)) {
+            $this->pathArray = $name;
+            return $this;
+        }
         self::$path_load_file = '/'. $name;
         return $this;
     }
 
+    public function work() {
+        if(empty($this->pathArray)) {
+            throw new \RuntimeException('Name function add() is not null', 500);
+            return;
+        }
+        foreach ($this->pathArray as $name=>$fileName) {
+            self::$path_load_file = '/'. $name;
+            $this->loadFile($fileName);
+        }
+    }
 }
 
-
 $configRouter = new ConfigRouter();
-$configRouter->add('api')->loadFile('api');
+
+$configRouter->add([
+    'web' => 'web',
+    'api' => 'api'
+])->work();
