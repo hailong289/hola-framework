bd313fc5673753085b41c9265cb8e96f79bf68ec
Hải Long <longdh2.dev@gmail.com>
2025-02-10 15:52:56 +0700

    refactor: clean code cli

--- cli.php
+++ cli.php
@@ -4,12 +4,9 @@ define('__DIR__ROOT', __DIR__);
 ini_set('error_reporting', E_STRICT);
 // load config
 $appRegister = new \Hola\Container\RegisterLoad();
-$appRegister->loadConfig();
-$appRegister->loadTimeZone();
-$appRegister->registerFolder([
-    'database'
-]);
+$appRegister->initCLI();
  // register command
 $application = new App\App();
 $application->register();
 $application->registerCommand();
+$application->runCLI();
