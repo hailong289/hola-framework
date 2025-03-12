bd519cab3275750e19af1deab7068f77898bdcbc
Hải Long <longdh2.dev@gmail.com>
2025-03-12 15:54:47 +0700

    change: update text error

--- test.php
+++ test.php
@@ -5,12 +5,12 @@ try {
     if (file_exists('vendor/autoload.php')) {
         require 'vendor/autoload.php';
     } else {
-        throw new \Exception("File vendor/autoload.php không tồn tại!");
+        throw new \Exception("File vendor/autoload.php doest not exist!");
     }
     if (file_exists('bootstrap.php')) {
         require_once "bootstrap.php";
     } else {
-        throw new \Exception("File bootstrap.php không tồn tại!");
+        throw new \Exception("File bootstrap.php doest not exist!");
     }
 
     $app = new App\App();
